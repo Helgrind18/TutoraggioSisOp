887d07da51ec3775f7e124e9bd43b5eedccd1bb0
helgrind <fiorepasquale2003@gmail.com>
2026-04-27 19:02:46 +0200

    Aggiunta soluzione sequenza AAB

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,53 @@
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    private static Semaphore mutex = new Semaphore(1);
+    private static Semaphore stampaA = new Semaphore(2);
+    private static Semaphore stampaB = new Semaphore(0);
+    private static int cntA = 0;
+    static class P1 extends Thread{
+        @Override
+        public void run() {
+            try {
+                stampaA.acquire();
+                System.out.printf("A");
+                mutex.acquire();
+                cntA++;
+                if (cntA == 2){
+                    stampaB.release();
+                    cntA = 0;
+                }
+                mutex.release();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    static class P2 extends Thread{
+        @Override
+        public void run() {
+            try {
+                stampaB.acquire();
+                System.out.printf("B");
+                stampaA.release(2);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        while (true) {
+            new P1().start();
+            new P2().start();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
